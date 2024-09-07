ce1034207a472ab3646af0e64d3ccdfb7ad759f6
Evgen Bratenkov <giertrut@mail.ru>
2024-09-07 16:15:51 +0300

    update migration

--- app/models/rooms.py
+++ app/models/rooms.py
@@ -5,10 +5,10 @@ from app.models import *
 class Room(Base):
     __tablename__ = 'rooms'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    hotel_id = Column(Integer, ForeignKey('hotels.id'))
-    name = Column(String)
-    description = Column(String)
-    price = Column(Integer)
-    services = Column(JSON)
-    quantity = Column(Integer)
+    hotel_id = Column(Integer, ForeignKey('hotels.id'), nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    services = Column(JSON, nullable=True)
+    quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
