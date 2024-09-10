7aefe969d35bd6a88303e6bbd11e9dadee1c7bd1
Evgen Bratenkov <giertrut@mail.ru>
2024-09-10 19:31:50 +0300

    added basesevices

--- app/services/basesevice.py
+++ app/services/basesevice.py
@@ -6,7 +6,19 @@ class BaseService:
     model = None
 
     @classmethod
-    async def find_all(cls, db):
-        query = select(cls.model)
+    async def find_by_id(cls, db, model_id):
+        query = select(cls.model).filter_by(id=model_id)
         result = await db.execute(query)
-        return result.mappings().all()
+        return result.scalar_one_or_none()
+
+    @classmethod
+    async def find_one_or_none(cls, db, **filter_by):
+        query = select(cls.model).filter_by(**filter_by)
+        result = await db.execute(query)
+        return result.scalar_one_or_none()
+
+    @classmethod
+    async def find_all(cls, db, **filter_by):
+        query = select(cls.model).filter_by(**filter_by)
+        result = await db.execute(query)
+        return result.scalars().all()
